5017231f675a446ae0e7115fc861661d8300133a
Andrei-Skok <specokok@gmail.com>
2023-04-13 01:14:32 +0300

    First parser_delete trash

--- parser_AV.py
+++ parser_AV.py
@@ -4,16 +4,16 @@ from unicodedata import normalize
 import pandas as pd
 
 av = requests.get('https://cars.av.by/citroen/c5')
-print(av) #смотрим ответ сервера
+print(av)  # смотрим ответ сервера
 soup_av = BeautifulSoup(av.content, 'html.parser')
-#парсим данные и форматируем кодировку
+# парсим данные и форматируем кодировку
 models = soup_av.findAll(class_="listing-item__title")
 models = [normalize('NFKC', str(model.text)) for model in models]
 prices_byn = soup_av.findAll(class_="listing-item__price")
 prices_byn = [normalize('NFKC', str(price.text)) for price in prices_byn]
 prices_usd = soup_av.findAll(class_="listing-item__priceusd")
 prices_usd = [normalize('NFKC', str(price.text)) for price in prices_usd]
-#удаление лишних символов из цены в USD и перевод к числовому типу
+# удаление лишних символов из цены в USD и перевод к числовому типу
 for price in prices_usd:
     price_formated = str()
     for symbol_ in price:
@@ -34,15 +34,13 @@ for param in params:
     mileage.append(normalize('NFKC', str(param[2].text)))
 links = [link.get('href') for link in soup_av.findAll(class_="listing-item__link")]
 
-#собираем словарь всех данных об авто
+# собираем словарь всех данных об авто
 total_dict = {'model': models, 'BYN': prices_byn, 'USD': prices_usd, 'year': years,
               'gearbox_type': gearbox_type, 'engine_capacity': engine_capacity, 'fuel_type': fuel_type,
               'body_type': body_type, 'mileage': mileage, 'link': links}
-#переводим словарь в DataFrame для удобства сортировки, записи в файл
+# переводим словарь в DataFrame для удобства сортировки, записи в файл
 data = pd.DataFrame(total_dict)
 data_sort_usd = data.sort_values(by='USD')
 
 with open('av.csv', 'w', encoding='UTF-8') as file_csv:
     data_sort_usd.to_csv(file_csv)
-# with open('av.json', 'w', encoding='UTF-8') as file_json:
-#     data.to_json(file_json)
